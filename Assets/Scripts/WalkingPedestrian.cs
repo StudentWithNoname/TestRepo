bea9bdd7c1f0093f34da2eeba3fee2cdd48189d4
Marcel Hendrik Müller <marcel_mueller86@gmx.de>
2024-06-05 22:01:54 +0200

    walking pedestrian added

---
+++ Assets/Scripts/WalkingPedestrian.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkingPedestrian : MonoBehaviour
+{
+    public float moveRadius = 30f;
+    public float interval = 15f; // 
+    private NavMeshAgent agent;
+    private float timer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = interval;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            SetNewRandomDestination();
+            timer = 0; 
+        }
+    }
+
+    void SetNewRandomDestination()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * moveRadius + transform.position;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, moveRadius, -1))
+        {
+            agent.SetDestination(hit.position);
+        }
+    }
+}
